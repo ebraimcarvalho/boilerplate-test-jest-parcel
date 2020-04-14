06fe70e8a0b3f2e72fe9a0e6e6982599973cc8ea
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-04-14 00:59:25 -0300

    test add and removeSNames

--- src/App.js
+++ src/App.js
@@ -3,3 +3,11 @@ export const hello = () => 'Hello World!';
 let text = document.createElement('h3');
 text.textContent = hello();
 document.body.appendChild(text);
+
+export const add = (num1, num2) => {
+  return (typeof num1 === 'number' && typeof num2 === 'number') ? num1 + num2 : null;
+};
+
+export const removeSNames = names => {
+  return names.filter(name => name.toLowerCase().charAt(0) !== 's');
+};
